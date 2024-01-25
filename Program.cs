07bb7e38b385f07f1f5fb87b7ce17d9b237e94cf
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-01-25 13:34:53 +0530

    Upload Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using my-code.Data;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddTransient<my-codeContext>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
