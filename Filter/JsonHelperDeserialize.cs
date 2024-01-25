5fb5d56d184933da6e19848cdb7e5025a1e5e349
Pankaj Tiwari <54172974+pankajkumar2701@users.noreply.github.com>
2024-01-25 13:34:34 +0530

    Upload JsonHelperDeserialize.cs

---
+++ Filter/JsonHelperDeserialize.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text.Json;
+
+namespace my - code . Filter{
+    public static class JsonHelper
+    {
+        public static T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
